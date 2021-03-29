0184e6fdb6687f6715084e022a8df34f12afd3b6
devsunset <devsunset@gmail.com>
2021-03-29 16:07:21 +0900

    dev

--- upbitapi/upbitapi.py
+++ upbitapi/upbitapi.py
@@ -579,7 +579,7 @@ class UpbitApi():
             else:
                 type_temp = 2
 
-            for(s in states):
+            for s in states:
                  if type_temp == 1:
                     if s not in ['wait', 'watch']:
                         logging.error('invalid state: %s' % s)
