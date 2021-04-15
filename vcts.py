363a179500acad24b9a1b6b9b262c7a8f8fec31a
devsunset <devsunset@gmail.com>
2021-04-15 16:52:34 +0900

    dev

--- vcts.py
+++ vcts.py
@@ -53,10 +53,15 @@ def continueGrowsChoice():
         data_json =  vctstrade.getTickerMarkets(best)
         for data in data_json:   
             # print(json.dumps(data, indent=4, sort_keys=True))
-            print('%10s' % data.get('market'),'market-name',data.get('trade_time_kst'),data.get('change'),data.get('trade_price'),data.get('change_price'))            
+            print('%10s' % data.get('market')
+            ,'market-name'
+            ,data.get('trade_time_kst')
+            ,data.get('change')
+            ,'%20f' % data.get('trade_price')
+            ,'%20f' % data.get('change_price'))         
         print('-----------------------')
-        break
         time.sleep(3) 
+        print('-----------------------')
         # market	종목 구분 코드	String
         # trade_date	최근 거래 일자(UTC)	String
         # trade_time	최근 거래 시각(UTC)	String
@@ -82,30 +87,6 @@ def continueGrowsChoice():
         # lowest_52_week_price	52주 신저가	Double
         # lowest_52_week_date	52주 신저가 달성일	String
         # timestamp	타임스탬프	Long
-
-    # "acc_trade_price": 79.84965309725581,
-    # "acc_trade_price_24h": 82.44701511,
-    # "acc_trade_volume": 28548413.13348581,
-    # "acc_trade_volume_24h": 29539273.44148463,
-    # "change": "RISE",
-    # "change_price": 2.7e-07,
-    # "change_rate": 0.1050583658,
-    # "high_price": 3.1e-06,
-    # "highest_52_week_date": "2021-04-14",
-    # "highest_52_week_price": 3.1e-06,
-    # "low_price": 2.48e-06,
-    # "lowest_52_week_date": "2021-01-03",
-    # "lowest_52_week_price": 4.5e-07,
-    # "market": "BTC-BFC",
-    # "opening_price": 2.59e-06,
-    # "prev_closing_price": 2.57e-06,
-    # "signed_change_price": 2.7e-07,
-    # "signed_change_rate": 0.1050583658,
-    # "timestamp": 1618440819083,
-    # "trade_date_kst": "20210415",
-    # "trade_price": 2.84e-06,
-    # "trade_time_kst": "075338",
-    # "trade_volume": 2339.80503287
 
 #################################################
 # main
