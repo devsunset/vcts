c82cc3099c380dc40524eb583f4d1d028daba3e1
devsunset <devsunset@gmail.com>
2021-04-19 12:44:07 +0900

    dev

--- vcts.py
+++ vcts.py
@@ -226,11 +226,9 @@ if __name__ == '__main__':
     # print('■■■■■■■■■■ - EXCHANGE API - 입금 - 입금 주소 생성 요청 : postExchangeDepositsGenerate_coin_address(currency)')
     # print(upbitapi.postExchangeDepositsGenerate_coin_address(currency))
 
-    print('■■■■■■■■■■ - EXCHANGE API - 입금 - 전체 입금 주소 조회 : getExchangeDepositsCoin_addresses()')
-    print(upbitapi.getExchangeDepositsCoin_addresses())
 
     print('■■■■■■■■■■ - EXCHANGE API - 입금 - 개별 입금 주소 조회 : getExchangeDepositsCoin_address(currency)')
-    print(upbitapi.getExchangeDepositsCoin_address('KRW'))
+    print(upbitapi.getExchangeDepositsCoin_address('KRW-AERGO'))
 
     # print('■■■■■■■■■■ - EXCHANGE API - 입금 - 원화 입금하기 : postExchangeDepositsKrw(currency)')
     # print(upbitapi.postExchangeDepositsKrw(currency))
