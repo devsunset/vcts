5c036a5aac2ddcb8727ae015acc101a9443bb982
devsunset <devsunset@gmail.com>
2021-04-21 16:57:30 +0900

    dev

--- vcts.py
+++ vcts.py
@@ -16,6 +16,7 @@
 
 import datetime
 from pandas import DataFrame
+from tabulate import tabulate
 import pandas as pd
 import numpy as np
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -56,7 +57,7 @@ SELL_DOWN_RATE = -1.45
 # biz function
 
 # market monitor
-def monitorMarkets(loop=False, looptime=3, sort='signed_change_rate', change=None, market=None, trade_price=None):
+def monitorMarkets(loop=False, looptime=3, sort='signed_change_rate', change=None):
     best = []
 
     ### TYPE ONE
@@ -75,58 +76,52 @@ def monitorMarkets(loop=False, looptime=3, sort='signed_change_rate', change=Non
 
     ###########################################################################################
     while True:
+        # get ticker market data
         df = vctstrade.getTickerMarkets(best).sort_values(by=sort, ascending=False)
-        print('---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-        print('%15s' % 'market'                
-                ,'%7s' % 'change'
-                ,'%12s' % '종가'
-                ,'%13s' % '변화액'
-                ,'%6s' % '변화율'
-                ,'%11s' % '고가-종가'
-                ,'%11s' % '고가-시가'
-                ,'%11s' % '고가-저가'
-                ,'%13s' % '시가'
-                ,'%13s' % '저가'
-                ,'%13s' % '고가'
-                # ,'%7s' % '신고일'
-                # ,'%12s' % '신고가'
-                # ,'% 7s' % '신저일'
-                # ,'%12s' % '신저가'
-                ,'%23s' %  'market'
-                )
-        print('---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-        for x in df.index:
 
-            if change is not None:
-                if change.find(df['change'][x]) == -1 :
-                    continue
+        # merge market info & ticker market data
+        df = pd.merge(df, markets, on = 'market')
 
-            if market is not None:
-                tmp = df['market'][x]
-                if tmp[:tmp.find('-')] not in market.split('|'):
-                    continue
+        # unused filed delete
+        # market info
+        del df['id']
+        del df['korean_name']
+        del df['english_name']
+        del df['market_warning']
+        # ticker market data
+        # del df['market'] #종목 구분 코드	
+        del df['trade_date'] 	#최근 거래 일자(UTC)	
+        del df['trade_time'] 	#최근 거래 시각(UTC)	
+        del df['trade_date_kst'] 	#최근 거래 일자(KST)	
+        del df['trade_time_kst'] 	#최근 거래 시각(KST)	
+        del df['trade_timestamp'] 	#최근 거래  타임스탬프	
+        # del df['opening_price'] 	#시가	
+        # del df['high_price'] 	#고가	
+        # del df['low_price'] 	#저가	
+        # del df['trade_price'] 	#종가	
+        # del df['prev_closing_price'] 	#전일 종가	
+        # del df['change'] 	#EVEN : 보합 RISE : 상승 FALL : 하락	
+        del df['change_price'] 	#변화액의 절대값	
+        del df['change_rate'] 	#변화율의 절대값	
+        # del df['signed_change_price'] 	#부호가 있는 변화액	
+        # del df['signed_change_rate'] 	#부호가 있는 변화율	
+        # del df['trade_volume'] 	#가장 최근 거래량	
+        del df['acc_trade_price'] 	#누적 거래대금(UTC 0시 기준)	
+        # del df['acc_trade_price_24h'] 	#24시간 누적 거래대금	
+        del df['acc_trade_volume'] 	#누적 거래량(UTC 0시 기준)	
+        # del df['acc_trade_volume_24h'] 	#24시간 누적 거래량	
+        del df['highest_52_week_price'] 	#52주 신고가	
+        del df['highest_52_week_date'] 	#52주 신고가 달성일	
+        del df['lowest_52_week_price'] 	#52주 신저가	
+        del df['lowest_52_week_date'] 	#52주 신저가 달성일	
+        del df['timestamp'] 	#타임스탬프	
 
-            if trade_price is not None:
-                if int(df['trade_price'][x]) > 1000 :
-                    continue
+        #  tabulate Below are all the styles that you can use :
+        # “plain” “simple” “github” “grid” “fancy_grid” “pipe” “orgtbl” “jira” “presto” “pretty” “psql” “rst” “mediawiki” “moinmoin” “youtrack” “html” “latex” “latex_raw” “latex_booktabs”  “textile”
 
-            print('%15s' % df['market'][x]
-                ,'%6s' % df['change'][x]
-                ,'%15f' % df['trade_price'][x]
-                ,'%15f' % df['signed_change_price'][x]
-                ,'%10f' % df['signed_change_rate'][x]
-                ,'%15f' % (float(df['high_price'][x]) - float(df['trade_price'][x])) 
-                ,'%15f' % (float(df['high_price'][x]) - float(df['opening_price'][x])) 
-                ,'%15f' % (float(df['high_price'][x]) - float(df['low_price'][x])) 
-                ,'%15f' % df['opening_price'][x]
-                ,'%15f' % df['low_price'][x]
-                ,'%15f' % df['high_price'][x]
-                # ,'%10s' % df['highest_52_week_date'][x]
-                # ,'%15f' % df['highest_52_week_price'][x]
-                # ,'%10s' % df['lowest_52_week_date'][x]
-                # ,'%15f' % df['lowest_52_week_price'][x]
-                ,'%20s' % vctstrade.getMarketName(df['market'][x])
-                )
+        # displaying the DataFrame
+        print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+
         if(loop == True):
             time.sleep(looptime)
         else:
@@ -342,17 +337,10 @@ if __name__ == '__main__':
     # vctstrade.loadMarketsCandlesMwdData()
 
     # moinitor markets info
-            # signed_change_rate 변화율
-            # trade_volume	가장 최근 거래량	
-            # acc_trade_price	누적 거래대금(UTC 0시 기준)	
-            # acc_trade_price_24h	24시간 누적 거래대금	
-            # acc_trade_volume	누적 거래량(UTC 0시 기준)	
-            # acc_trade_volume_24h	24시간 누적 거래량	
-    # monitorMarkets(loop=False,looptime=3,sort='signed_change_rate',change='RISE',market='KRW',trade_price=1000)
-    # monitorMarkets(loop=True,looptime=5,sort='signed_change_rate',market='KRW')
+    monitorMarkets(loop=False,looptime=5,sort='signed_change_rate')
 
     # watch jump market info 
-    watchJumpMarkets(looptime=5, period=7, market='KRW', trade_price = 1000)
+    # watchJumpMarkets(looptime=5, period=7)
 
     # scheduler = BlockingScheduler()
     # scheduler.add_job(daemon_process, 'interval', seconds=config.INTERVAL_SECONDS)
